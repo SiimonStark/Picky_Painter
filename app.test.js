aebb839dcc603e2d1e369fefd9162e062a8c29e2
SiimonStark <justinpyktel@gmail.com>
2019-07-02 13:41:51 -0600

    maybe

--- app.test.js
+++ app.test.js
@@ -1,8 +1,8 @@
 // import request from 'supertest';
-import app from './app';
+// import app from './app';
 const environment = process.env.NODE_ENV || 'test';
 const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration);
+// const database = require('knex')(configuration);
 
 describe('Server', () => {
   beforeEach(async () => {
