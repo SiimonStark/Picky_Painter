dbea0b802b24843f20546f4dce7a1045b37fcec6
SiimonStark <justinpyktel@gmail.com>
2019-07-02 13:38:02 -0600

    initial test

---
+++ app.test.js
@@ -0,0 +1,48 @@
+import request from 'supertest';
+import app from './app';
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+describe('Server', () => {
+  beforeEach(async () => {
+
+  })
+
+
+  describe('init', () => {
+    it.skip('should return a 200 status', async () => {
+      
+    })
+  })
+
+  describe('GET /students', () => {
+    it.skip('should return all the students in the DB', async () => {
+      
+    })
+  })
+
+  describe('GET /students/:id', () => {
+    it.skip('should get a single student with the id in the params', async () => {
+      
+    })
+  })
+
+  describe('POST /students', () => {
+    it.skip('should Post a single student to the database', async () => {
+      
+    })
+  })
+
+  describe('PUT /students/:id', () => {
+    it('should change one of the current students in the database', async () => {
+      
+    })
+  })
+
+  describe('DELETE /students', () => {
+    it('should remove the student from database by id', async () => {
+      
+    })
+  })
+})
---
+++ server.js
@@ -0,0 +1,7 @@
+const app = require('./app.js');
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+    console.log(`Palette-picker is running http://localhost:${app.get('port')}`)
+});
