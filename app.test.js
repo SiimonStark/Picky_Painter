ec5f7bf6562e0bccf326aa39f52251c76034654d
SiimonStark <justinpyktel@gmail.com>
2019-07-02 13:40:33 -0600

    passing test file

--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
-import request from 'supertest';
+// import request from 'supertest';
 import app from './app';
 const environment = process.env.NODE_ENV || 'test';
 const configuration = require('./knexfile')[environment];
